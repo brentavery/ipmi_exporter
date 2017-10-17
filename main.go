fa554097ec13f0f10bc2919641eadc77f5dacca3
Brent Avery <brent.avery0@gmail.com>
2017-10-17 22:03:04 +1100

    Add hostname, username, and password options for ipmitool

--- main.go
+++ main.go
@@ -17,7 +17,10 @@ import (
 var (
 	listenAddress = flag.String("web.listen", ":9289", "Address on which to expose metrics and web interface.")
 	metricsPath   = flag.String("web.path", "/metrics", "Path under which to expose metrics.")
-	ipmiBinary    = flag.String("ipmi.path", "ipmitool", "Path to the ipmi binary")
+	ipmiBinary    = flag.String("ipmi.path", "ipmitool", "Path to ipmi binar")
+	ipmiHost      = flag.String("ipmi.host", "", "IPMI server address")
+	ipmiUser      = flag.String("ipmi.user", "", "IPMI server username")
+	ipmiPass      = flag.String("ipmi.password", "", "IPMI server password")
 	showVersion   = flag.Bool("version", false, "Show version information and exit")
 )
 
@@ -32,6 +35,20 @@ func main() {
 		fmt.Fprintln(os.Stdout, version.Print("ipmi_exporter"))
 		os.Exit(0)
 	}
+ 
+	if *ipmiHost != "" {
+		*ipmiBinary += " -H " + string(*ipmiHost)
+	}
+
+	if *ipmiUser != "" {
+		*ipmiBinary += " -U" + string(*ipmiUser)
+	}
+
+	if *ipmiPass != "" {
+		*ipmiBinary += " -P" + string(*ipmiPass)
+	}
+
+	//log.Infoln("ipmi command", *ipmiBinary)
 
 	log.Infoln("Starting IPMI Exporter", version.Info())
 	log.Infoln("Build context", version.BuildContext())
